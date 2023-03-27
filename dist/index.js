7e82b880298a0aea460f9f330862ae5b6c78920e
IvanB <ivar110kg@gmail.com>
2023-03-27 18:02:06 +0400

    Add variable init

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hello world');
+let a;
+a = 10.6;
+const b = true;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log('Hello world');
+let age: number;
+age = 10.6;
+const b:boolean = true;
+
+if (age > 50) {
+    age -= 25;
+}
