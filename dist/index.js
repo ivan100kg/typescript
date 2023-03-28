40e51fed16b45e6460dc6d2125ac61276666ca95
IvanB <ivar110kg@gmail.com>
2023-03-28 17:35:41 +0400

    Add datatypes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
-console.log('Hello world');
-let a;
-a = 10.6;
-const b = true;
+let num = 12345;
+let txt = 'TS';
+let bol = true;
+let xyz;
+let arr = [1, 2];
+let ar2 = [1, '2'];
+arr.forEach(n => console.log(n.toPrecision(3)));
+let tup;
+var Si;
+(function (Si) {
+    Si[Si["One"] = 1] = "One";
+    Si[Si["Two"] = 2] = "Two";
+})(Si || (Si = {}));
+;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-console.log('Hello world');
-let age: number;
-age = 10.6;
-const b:boolean = true;
+let num: number = 12345;    // number
+let txt: string = 'TS';     // string
+let bol: boolean = true;    // boolean
+let xyz: any;               // any type like JS
 
-if (age > 50) {
-    age -= 25;
-}
+// array
+let arr: number[] = [1,2];  // array
+let ar2: any[] = [1,'2'];   // like JS array
+// спец методы типов без проверки на тип
+console.log(arr.forEach(n => n.toFixed(2)));
+
+// tupple - массив только опред числа эл, опред типов
+let tup: [number, boolean] = [1, false];
+tup[0].toFixed(2);          // спец методы типов
+
+// enum
+enum Si {One = 1, Two};     // enum, 1 - custom start number 
